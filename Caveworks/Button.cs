b15b4a497e8e0593ca06ce362454649264c5597a
David Sobek <Sodavid2@seznam.cz>
2024-10-27 15:13:03 +0100

    removed all code, draw RGB rectangles

---
+++ Caveworks/Button.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using SharpDX.Direct2D1;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caveworks
+{
+    public class Button
+    {
+        private Texture2D texture;
+
+        Vector2 position;
+        Vector2 size;
+        string text;
+
+        public Button(Vector2 position, Vector2 size, string text)
+        {
+            this.position = position;
+            this.size = size;
+            this.text = text;
+        }
+
+        public void Update()
+        {
+            return;
+        }
+
+        public void Draw()
+        {
+            return;
+        }
+    }
+}
--- Caveworks/Game1.cs
+++ Caveworks/Game1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Xml;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -8,95 +9,42 @@ namespace Caveworks
 {
     public class Game1 : Game
     {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
-
-        Texture2D playerTexture;
-        Vector2 playerPositon;
-        Vector2 playerOrigin;
-        float playerSpeed;
-        float playerRotation;
+        public GraphicsDeviceManager graphics;
+        public SpriteBatch spriteBatch;
+        public Texture2D whiteRectangle;
 
         public Game1()
         {
-            _graphics = new GraphicsDeviceManager(this);
+            graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
 
-        protected override void Initialize()
+        protected override void Initialize() // TODO: Add your initialization logic here
         {
-            // TODO: Add your initialization logic here
+            spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            base.Initialize();
-
-            playerPositon = new Vector2(_graphics.PreferredBackBufferWidth/2, _graphics.PreferredBackBufferHeight/2);
-            playerOrigin = new Vector2(playerTexture.Width/2, playerTexture.Height/2);
-            playerSpeed = 100f;
-            playerRotation = 0;
+            whiteRectangle = new Texture2D(GraphicsDevice, 1, 1);
+            whiteRectangle.SetData(new[] { Color.White });
         }
 
-        protected override void LoadContent()
+        protected override void LoadContent() // TODO: use this.Content to load your game content here
         {
-            // TODO: use this.Content to load your game content here
 
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            playerTexture = Content.Load<Texture2D>("BlueMan");
-            
-
-            
         }
 
-        protected override void Update(GameTime gameTime)
+        protected override void Update(GameTime gameTime) // TODO: Add your update logic here
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
 
-            float playerMoveDistance = playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-            if (Keyboard.GetState().IsKeyDown(Keys.A))
-            {
-                playerPositon.X -= playerMoveDistance;
-            }
-
-            if (Keyboard.GetState().IsKeyDown(Keys.D))
-            {
-                playerPositon.X += playerMoveDistance;
-            }
-
-            if (Keyboard.GetState().IsKeyDown(Keys.W))
-            {
-                playerPositon.Y -= playerMoveDistance;
-            }
-
-            if (Keyboard.GetState().IsKeyDown(Keys.S))
-            {
-                playerPositon.Y += playerMoveDistance;
-            }
-
-            Vector2 mousePosition = new Vector2(Mouse.GetState().X, Mouse.GetState().Y);
-            Vector2 playerDirection = new Vector2(mousePosition.X - playerPositon.X, mousePosition.Y - playerPositon.Y);
-            Debug.WriteLine(playerDirection.X + " " + playerDirection.Y);
-            playerRotation = (float)Math.Atan2(playerDirection.Y, playerDirection.X) + (float)Math.PI/2;
-
-
-            // TODO: Add your update logic here
-
-            base.Update(gameTime);
         }
 
-        protected override void Draw(GameTime gameTime)
+        protected override void Draw(GameTime gameTime) // TODO: Add your drawing code here
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            _spriteBatch.Begin(samplerState: SamplerState.PointWrap);
-            _spriteBatch.Draw(playerTexture, playerPositon, null,  Color.White, playerRotation, playerOrigin, 2f, SpriteEffects.None, 0f);
-            _spriteBatch.End();
-
-            // TODO: Add your drawing code here
-
-            base.Draw(gameTime);
+            spriteBatch.Begin();
+            spriteBatch.Draw(whiteRectangle, new Rectangle(100, 100, 200, 200), Color.FromNonPremultiplied(new Vector4(255, 0, 0, 0.5f)));
+            spriteBatch.Draw(whiteRectangle, new Rectangle(150, 200, 200, 200), Color.FromNonPremultiplied(new Vector4(0, 255, 0, 0.5f)));
+            spriteBatch.Draw(whiteRectangle, new Rectangle(200, 150, 200, 200), Color.FromNonPremultiplied(new Vector4(0, 0, 255, 0.5f)));
+            spriteBatch.End();
         }
     }
 }
