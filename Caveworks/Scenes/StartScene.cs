a96e8a459c81e4619ede7bb5c36eb807a1a97e30
David Sobek <Sodavid2@seznam.cz>
2025-04-22 18:40:02 +0200

    minor changes

--- Caveworks/Scenes/StartScene.cs
+++ Caveworks/Scenes/StartScene.cs
@@ -11,7 +11,7 @@ namespace Caveworks
         readonly static Slider worldSizeSlider = new Slider(new Vector2(350, 20), Globals.UIButtonColor, 2, 8, 16, 16, 0);
 
         readonly static TextBox researchMultText = new TextBox(new Vector2(350, 60), Globals.UIButtonColor, 2, "Research cost:", Fonts.LargeFont);
-        readonly static Slider researchMultSlider = new Slider(new Vector2(350, 20), Globals.UIButtonColor, 2, 0.1f, 1, 4, 1);
+        readonly static Slider researchMultSlider = new Slider(new Vector2(350, 20), Globals.UIButtonColor, 2, 0.1f, 1, 2, 1);
 
         readonly static Button startButton = new Button(new Vector2(200, 60), Globals.UIButtonColor, 2, "Start", Fonts.LargeFont);
 
